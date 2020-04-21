2aa72d8eba1b27d83880e48cbb32f2b1cb324f29
Regina-a <gomezpr.2005@gmail.com>
2020-04-21 18:24:07 -0500

    Practica con kachyz

---
+++ funciones.js
@@ -0,0 +1,16 @@
+function cambiarcolor(){
+console.log ('Dieron click')
+
+
+
+ let etiqueta = document.getElementById('aqui')
+ etiqueta.classList.add("letrasrojas")
+}
+function subrayar(){
+console.log ('Dieron click')
+
+
+
+ let etiqueta = document.getElementById('aqui')
+ etiqueta.classList.add("subra")
+}
