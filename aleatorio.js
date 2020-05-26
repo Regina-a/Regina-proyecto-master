949884798ecfa1b6524d03e1549a6f897bbc820f
Regina-a <gomezpr.2005@gmail.com>
2020-05-26 17:18:21 -0500

    hoy

--- aleatorio.js
+++ aleatorio.js
@@ -1,7 +1,15 @@
 function inicio(){
 console.log('fghj');
 let numeroAleatorio = Math.floor(Math.random() * 10);
-console.log(numeroAleatorio) 
+console.log(numeroAleatorio)
 }
 let intento = prompt('Adivina el numero')
 console.log(intento)
+
+a
+if (intento == numeroAleatorio){
+  alert("estas bien ")
+} else{
+  alert("estas mal")
+}
+}
